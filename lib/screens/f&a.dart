45fc08ce158f5b699d6642d07c5be93b824da0ee
Ali Safarli <xeligamer.2002@gmail.com>
2022-12-25 18:10:28 +0300

    F&A Screen

--- lib/screens/f&a.dart
+++ lib/screens/f&a.dart
@@ -1,13 +1,205 @@
+import 'package:city_guide_app/variables.dart';
 import 'package:flutter/material.dart';
+import 'package:city_guide_app/fadeanimation.dart';
 
-class FAScreen extends StatelessWidget {
-  const FAScreen({super.key});
+class FAScreen extends StatefulWidget {
+  @override
+  State<FAScreen> createState() => _FAScreenState();
+}
 
+class _FAScreenState extends State<FAScreen> {
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
+    return Scaffold(
+      backgroundColor: themeColor,
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Container(
+              height: 300,
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                    image: AssetImage('assets/images/hotel.webp'),
+                    fit: BoxFit.cover),
+              ),
+              child: Container(
+                decoration: BoxDecoration(
+                    gradient:
+                        LinearGradient(begin: Alignment.bottomRight, colors: [
+                  Colors.black.withOpacity(.8),
+                  Colors.black.withOpacity(.2),
+                ])),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: <Widget>[
+                    FadeAnimation(
+                        1,
+                        Text(
+                          "What you would like to find?",
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 40,
+                              fontWeight: FontWeight.bold),
+                        )),
+                    SizedBox(
+                      height: 30,
+                    ),
+                    FadeAnimation(
+                        1.3,
+                        Container(
+                          padding: EdgeInsets.symmetric(vertical: 3),
+                          margin: EdgeInsets.symmetric(horizontal: 40),
+                          height: 50,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(50),
+                            color: Colors.white,
+                          ),
+                          child: TextField(
+                            decoration: InputDecoration(
+                                border: InputBorder.none,
+                                prefixIcon: Icon(
+                                  Icons.search,
+                                  color: Colors.grey,
+                                ),
+                                hintStyle:
+                                    TextStyle(color: Colors.grey, fontSize: 15),
+                                hintText: "Search for hotels, restaurants ..."),
+                          ),
+                        )),
+                    SizedBox(
+                      height: 30,
+                    )
+                  ],
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            Container(
+              padding: EdgeInsets.symmetric(horizontal: 20),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  FadeAnimation(
+                      1,
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text("Restaurants", style: topicTextStyle),
+                          Text('>', style: topicTextStyle),
+                        ],
+                      )),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  FadeAnimation(
+                      1.4,
+                      Container(
+                        height: 200,
+                        child: ListView(
+                          scrollDirection: Axis.horizontal,
+                          children: <Widget>[
+                            makeItem(
+                                image: 'assets/images/restaurant1.jpg',
+                                title: 'Aquarium Mezzes'),
+                            makeItem(
+                                image: 'assets/images/restaurant2.jpg',
+                                title: 'Mengoli Burgers'),
+                            makeItem(
+                                image: 'assets/images/restaurant3.jpg',
+                                title: 'Adabeyi Balik'),
+                            makeItem(
+                                image: 'assets/images/restaurant4.jpg',
+                                title: 'Qoch Steak')
+                          ],
+                        ),
+                      )),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  FadeAnimation(
+                      1,
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            "Hotels",
+                            style: topicTextStyle,
+                          ),
+                          Text(
+                            '>',
+                            style: topicTextStyle,
+                          ),
+                        ],
+                      )),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  FadeAnimation(
+                      1.4,
+                      Container(
+                        height: 200,
+                        child: ListView(
+                          scrollDirection: Axis.horizontal,
+                          children: <Widget>[
+                            makeItem(
+                                image: 'assets/images/hotel1.jpg',
+                                title: 'Marriott'),
+                            makeItem(
+                                image: 'assets/images/hotel2.jpg',
+                                title: 'Swissotel Buyuk Efes'),
+                            makeItem(
+                                image: 'assets/images/hotel3.jpg',
+                                title: 'Park Inn by Radisson'),
+                            makeItem(
+                                image: 'assets/images/hotel4.jpg',
+                                title: 'Coordinat Suits')
+                          ],
+                        ),
+                      )),
+                  SizedBox(
+                    height: 80,
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget makeItem({image, title}) {
+    return AspectRatio(
+      aspectRatio: 1 / 1,
       child: Container(
-        child: Text('F&A'),
+        margin: EdgeInsets.only(right: 15),
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(20),
+            image:
+                DecorationImage(image: AssetImage(image), fit: BoxFit.cover)),
+        child: Container(
+          padding: EdgeInsets.all(20),
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(20),
+              gradient: LinearGradient(begin: Alignment.bottomRight, colors: [
+                Colors.black.withOpacity(.8),
+                Colors.black.withOpacity(.2),
+              ])),
+          child: Align(
+            alignment: Alignment.bottomLeft,
+            child: Text(
+              title,
+              style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold),
+            ),
+          ),
+        ),
       ),
     );
   }
--- lib/variables.dart
+++ lib/variables.dart
@@ -1,3 +1,9 @@
 import 'package:flutter/material.dart';
 
-const Color themeColor = Color(0xFFF7F6EE);
+const Color themeColor = Color.fromARGB(255, 252, 250, 237);
+
+//F&A Screen
+const TextStyle topicTextStyle = TextStyle(
+    fontWeight: FontWeight.bold,
+    color: Color.fromARGB(255, 48, 48, 48),
+    fontSize: 20);
